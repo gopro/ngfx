1a72c22d4395c96f7b1b045ed49710ab3e5eaacf
Jeff Moguillansky <jmoguillansky@gopro.com>
2021-05-12 17:24:37 -0700

    build: remove vcpkg dependency

--- build_scripts/windows/set_env.ps1
+++ build_scripts/windows/set_env.ps1
@@ -1,8 +1,4 @@
 #Set-PSDebug -Trace 1
-if (-not (Test-Path env:VCPKG_DIR)) {
-    $env:VCPKG_DIR = 'C:\vcpkg'
-}
-
 $env:OS='Windows'
 $CURRENT_DIR=$PWD.tostring()
 $env:PKG_CONFIG=$CURRENT_DIR+'\external\windows\bin\pkgconf.exe'
@@ -10,4 +6,4 @@ $env:PKG_CONFIG=$CURRENT_DIR+'\external\windows\bin\pkgconf.exe'
 $env:PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1
 $env:PKG_CONFIG_ALLOW_SYSTEM_LIBS=1
 $env:PKG_CONFIG_DONT_DEFINE_PREFIX=1
-$env:PKG_CONFIG_PATH=$VCPKG_DIR+'\installed\x64-windows\lib\pkgconfig;'+$CURRENT_DIR+'\external\windows\lib\pkgconfig'
+$env:PKG_CONFIG_PATH=$CURRENT_DIR+'\external\windows\lib\pkgconfig'
