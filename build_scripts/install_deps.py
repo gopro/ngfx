b36c6313e3930171fe8fb3a8b202aaf7d909e50b
lbrownell-gpsw <lbrownell@gopro.com>
2021-09-22 17:14:42 -0700

    install_deps, what?

--- build_scripts/install_deps.py
+++ build_scripts/install_deps.py
@@ -2,9 +2,11 @@ from common import *
 
 os.chdir(EXTERNAL_DIR)
 
+PKGS = os.environ.get('PKGS')
+
 if not PKGS:
     PKGS = env('PKGS', 'all')
-    
+
 print("PKGS: %s" % PKGS)
 
 ninja_template = read_file(f'{SCRIPT_DIR}/install_deps_{OS_LOWER_CASE}.ninja.in')
