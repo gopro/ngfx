166d0f2ec9c8d611accb8193085c418d83e911fa
Jeff Moguillansky <jmoguillansky@gopro.com>
2022-04-18 09:02:08 -0700

    D3DTexture: remove redundant code

--- src/ngfx/porting/d3d/D3DTexture.cpp
+++ src/ngfx/porting/d3d/D3DTexture.cpp
@@ -559,7 +559,7 @@ void D3DTexture::changeLayout(CommandBuffer *commandBuffer,
         break;
     case IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL:
         resourceState = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE |
-            D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE ;
+            D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE;
         break;
     case IMAGE_LAYOUT_GENERAL:
         resourceState = D3D12_RESOURCE_STATE_COMMON;
@@ -569,10 +569,6 @@ void D3DTexture::changeLayout(CommandBuffer *commandBuffer,
         break;
     };
     resourceBarrierTransition(d3d(commandBuffer), resourceState);
-     if (imageLayout == IMAGE_LAYOUT_UNORDERED_ACCESS) {
-        CD3DX12_RESOURCE_BARRIER resourceBarrier = CD3DX12_RESOURCE_BARRIER::UAV(v.Get());
-        D3D_TRACE(d3d(commandBuffer)->v->ResourceBarrier(1, &resourceBarrier));
-    }
 }
 void D3DTexture::resourceBarrierTransition(D3DCommandList* cmdList,
     D3D12_RESOURCE_STATES newState,
