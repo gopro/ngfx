9813cd9490ba9d03b7d557016993949f7eb35809
Jeff Moguillansky <jmoguillansky@gopro.com>
2022-05-25 09:18:25 -0700

    D3DGraphicsContext: set depth format candidates

--- src/ngfx/porting/d3d/D3DGraphicsContext.cpp
+++ src/ngfx/porting/d3d/D3DGraphicsContext.cpp
@@ -32,7 +32,7 @@ void D3DGraphicsContext::create(const char *appName, bool enableDepthStencil,
   HRESULT hResult;
   this->debug = debug;
   this->enableDepthStencil = enableDepthStencil;
-  depthFormat = PIXELFORMAT_D16_UNORM;
+
   UINT dxgiFactoryFlags = 0;
   if (debug) {
     ComPtr<ID3D12Debug1> debugController;
@@ -57,6 +57,9 @@ void D3DGraphicsContext::create(const char *appName, bool enableDepthStencil,
       filter.AllowList.pSeverityList = severityList;
       infoQueue->PushStorageFilter(&filter);
   }
+  std::vector<DXGI_FORMAT> depthFormatCandidates = { DXGI_FORMAT_D32_FLOAT_S8X24_UINT, DXGI_FORMAT_D24_UNORM_S8_UINT, DXGI_FORMAT_D16_UNORM };
+  depthFormat = PixelFormat(findSupportedFormat(depthFormatCandidates, D3D12_FORMAT_SUPPORT1_DEPTH_STENCIL));
+
   d3dCommandQueue.create(this);
   createDescriptorHeaps();
   d3dCopyCommandList.create(d3dDevice.v.Get());
@@ -65,6 +68,16 @@ void D3DGraphicsContext::create(const char *appName, bool enableDepthStencil,
   d3dTimestampResultBuffer.create(this, 2 * sizeof(uint64_t));
 }
 
+DXGI_FORMAT D3DGraphicsContext::findSupportedFormat(const std::vector<DXGI_FORMAT>& formats, D3D12_FORMAT_SUPPORT1 formatSupport1) {
+    HRESULT hResult;
+    for (const auto format : formats) {
+        D3D12_FEATURE_DATA_FORMAT_SUPPORT formatSupport = { format };
+        V(d3dDevice.v->CheckFeatureSupport(D3D12_FEATURE_FORMAT_SUPPORT, &formatSupport, sizeof(formatSupport)));
+        if (formatSupport.Support1 & formatSupport1)
+            return format;
+    }
+    return DXGI_FORMAT_UNKNOWN;
+}
 void D3DGraphicsContext::createDescriptorHeaps() {
   d3dRtvDescriptorHeap.create(d3dDevice.v.Get(), D3D12_DESCRIPTOR_HEAP_TYPE_RTV,
                               MAX_DESCRIPTORS, D3D12_DESCRIPTOR_HEAP_FLAG_NONE);
