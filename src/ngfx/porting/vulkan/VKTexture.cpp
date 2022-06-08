d2c040b88521e8376b2c35b90aa463d74689a9c1
Jeff Moguillansky <jmoguillansky@gopro.com>
2022-06-08 13:45:05 -0700

    VKGraphicsCore: add format D32_SFLOAT

--- src/ngfx/porting/vulkan/VKTexture.cpp
+++ src/ngfx/porting/vulkan/VKTexture.cpp
@@ -119,6 +119,7 @@ VkImageAspectFlags VKTexture::getImageAspectFlags(VkFormat format) {
     switch (format) {
     case VK_FORMAT_D16_UNORM:
     case VK_FORMAT_X8_D24_UNORM_PACK32:
+    case VK_FORMAT_D32_SFLOAT:
         return VK_IMAGE_ASPECT_DEPTH_BIT;
     case VK_FORMAT_D24_UNORM_S8_UINT:
     case VK_FORMAT_D32_SFLOAT_S8_UINT:
