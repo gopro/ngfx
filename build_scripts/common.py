312be60b8dffc9bc62d3c13f2583af93f28ed8ef
Jeff Moguillansky <jmoguillansky@gopro.com>
2022-06-21 09:34:18 -0700

    build_scripts/common: upgrade to Visual Studio 2022

--- build_scripts/common.py
+++ build_scripts/common.py
@@ -54,11 +54,10 @@ INSTALL_PREFIX = env('INSTALL_PREFIX', op.join(os.getcwd(), 'install'))
 INSTALL_INCLUDEDIR = env('INSTALL_INCLUDEDIR', 'include')
 INSTALL_LIBDIR = env('INSTALL_LIBDIR', 'lib')
 
-CMAKE_GENERATOR = env('CMAKE_GENERATOR', { 'Windows': 'Visual Studio 16 2019', 'Linux': 'Ninja', 'Darwin': 'Xcode' }[OS])
+CMAKE_GENERATOR = env('CMAKE_GENERATOR', { 'Windows': 'Visual Studio 17 2022', 'Linux': 'Ninja', 'Darwin': 'Xcode' }[OS])
 NGFX_GRAPHICS_BACKEND = env('NGFX_GRAPHICS_BACKEND', { 'Windows': 'Direct3D12', 'Linux': 'Vulkan', 'Darwin': 'Metal' }[OS].upper())
 CMAKE = 'cmake.exe' if OS == 'Windows' else 'cmake'
 if OS == 'Windows':
-    env('CMAKE_GENERATOR', 'Visual Studio 16 2019')
     CMAKE_SYSTEM_VERSION = env('CMAKE_SYSTEM_VERSION', '10.0.22000.0')
     env('SHADERC_VERSION', '2020.3')
     VCPKG_DIR = env('VCPKG_DIR', 'C:\\vcpkg')
