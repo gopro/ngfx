90af62b20cb6b2e04a429984a26c292d90fc9868
Jeff Moguillansky <jmoguillansky@gopro.com>
2022-06-29 13:32:17 -0700

    FilterOp: add sampler

--- src/ngfx/graphics/FilterOp.cpp
+++ src/ngfx/graphics/FilterOp.cpp
@@ -25,10 +25,15 @@ FilterOp::FilterOp(GraphicsContext *ctx, Graphics *graphics, uint32_t dstWidth,
                    uint32_t dstHeight)
     : DrawOp(ctx) {
   uint32_t w = dstWidth, h = dstHeight, size = w * h * 4;
+  SamplerDesc samplerDesc = {
+            FILTER_LINEAR, FILTER_LINEAR, FILTER_LINEAR,
+            CLAMP_TO_EDGE, CLAMP_TO_EDGE, CLAMP_TO_EDGE
+  };
   outputTexture.reset(Texture::create(
       ctx, graphics, nullptr, PIXELFORMAT_RGBA8_UNORM, size, w, h, 1, 1,
       ImageUsageFlags(IMAGE_USAGE_SAMPLED_BIT | IMAGE_USAGE_TRANSFER_DST_BIT |
-                      IMAGE_USAGE_COLOR_ATTACHMENT_BIT)));
+                      IMAGE_USAGE_COLOR_ATTACHMENT_BIT),
+      TEXTURE_TYPE_2D, false, 1, &samplerDesc));
   outputFramebuffer.reset(Framebuffer::create(ctx->device,
                                               ctx->defaultOffscreenRenderPass,
                                               {{outputTexture.get()}}, w, h));
