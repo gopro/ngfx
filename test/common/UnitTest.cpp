56a449ef176e930831e478b8e3b2ab8f4a2058c0
Jeff Moguillansky <jmoguillansky@gopro.com>
2022-06-29 13:36:26 -0700

    test: add unit tests...

---
+++ test/common/UnitTest.cpp
@@ -0,0 +1,39 @@
+#include "UnitTest.h"
+#include "ngfx/graphics/ImageUtil.h"
+#include "ngfx/graphics/TextureUtil.h"
+#include "ngfx/core/FileUtil.h"
+using namespace ngfx;
+using namespace std;
+namespace fs = std::filesystem;
+
+UnitTest::UnitTest(std::string testName, int outputWidth, int outputHeight) :
+		testName(testName), outputWidth(outputWidth), outputHeight(outputHeight) {
+	ctx.reset(GraphicsContext::create(testName.c_str(), false));
+	surface.reset(new Surface(outputWidth, outputHeight, true));
+	graphics.reset(Graphics::create(ctx.get()));
+	ctx->setSurface(surface.get());
+	genRefs = getenv("GEN_REFS") ? true : false;
+}
+int UnitTest::run() {
+	auto commandBuffer = ctx->drawCommandBuffer();
+	commandBuffer->begin();
+	op->apply(ctx.get(), commandBuffer, graphics.get());
+	commandBuffer->end();
+	ctx->queue->submit(commandBuffer);
+	ctx->queue->waitIdle();
+	fs::path refFilePath = NGFX_TEST_DATA_DIR "/ref/" + testName + ".png";
+	if (genRefs) {
+		TextureUtil::storePNG(refFilePath.string(), op->outputTexture.get());
+		return 0;
+	}
+	fs::path filePath = "out/" + testName + ".png";
+	TextureUtil::storePNG(filePath.string(), op->outputTexture.get());
+	ImageData textureData;
+	TextureUtil::download(op->outputTexture.get(), textureData);
+	ImageData refImageData;
+	ImageUtil::load(refFilePath.string(), refImageData);
+	if (textureData.size != refImageData.size) {
+		return 1;
+	}
+	return memcmp(textureData.data, refImageData.data, refImageData.size);
+}
