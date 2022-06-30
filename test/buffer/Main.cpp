70bbd1c5792c8e437352d1a396f26aac631d88dd
Jeff Moguillansky <jmoguillansky@gopro.com>
2022-06-30 09:13:18 -0700

    fixup! test: add unit tests...

--- test/buffer/Main.cpp
+++ test/buffer/Main.cpp
@@ -1,5 +1,129 @@
-#include <iostream>
+/*
+ * Copyright 2022 GoPro Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
-int main() {
+#include <string>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <vector>
+#include "test/common/UnitTest.h"
+#include "ngfx/graphics/BufferUtil.h"
+#include "ngfx/drawOps/DrawColorOp.h"
+using namespace std;
+using namespace ngfx;
+using namespace glm;
+
+enum BufferTestMode { VERTEX, INDEX, UNIFORM, STORAGE, INSTANCING,
+    UPLOAD, UPLOAD_SUBREGION, DOWNLOAD, DOWNLOAD_SUBREGION, MAP
+};
+const vector<string> BufferTestModeStr = { "vertex", "index", "uniform", "storage",
+    "instancing", "upload", "upload_subregion", "download", "download_subregion", "map" };
+
+static string toString(BufferTestMode mode) {
+    return BufferTestModeStr[mode];
+}
+static BufferTestMode toBufferTestMode(string v) {
+    return BufferTestMode(find(BufferTestModeStr.begin(), BufferTestModeStr.end(), v) - BufferTestModeStr.begin());
+}
+
+class VertexBufferTestOp : public FilterOp {
+public:
+    VertexBufferTestOp(GraphicsContext* ctx, Graphics* graphics, int w, int h)
+        : FilterOp(ctx, graphics, w, h) {
+        int numSteps = 50;
+        vector<vec2> pos(numSteps);
+        auto it = pos.begin();
+        for (int j = 0; j < numSteps; j++) {
+            float t = j / float(numSteps - 1);
+            float x0 = -1.0f + 2.0f * t;
+            float y0 = sin(2 * M_PI * t);
+            *it++ = vec2(x0, y0);
+        }
+        op = make_unique<DrawColorOp>(ctx, pos, vec4(1, 0, 0, 1),
+            [&](GraphicsPipeline::State& state) {
+                state.primitiveTopology = PRIMITIVE_TOPOLOGY_LINE_STRIP;
+        });
+    }
+    void draw(CommandBuffer* commandBuffer, Graphics* graphics) override {
+        op->draw(commandBuffer, graphics);
+    }
+    std::unique_ptr<DrawColorOp> op;
+};
+class IndexBufferTestOp : public FilterOp {
+
+};
+class UniformBufferTestOp : public FilterOp {
+
+};
+class StorageBufferTestOp : public FilterOp {
+
+};
+class InstancingTestOp : public FilterOp {
+
+};
+class BufferUploadTestOp : public FilterOp {
+
+};
+class BufferUploadSubregionTestOp : public FilterOp {
+
+};
+class BufferDownloadSubregionTestOp : public FilterOp {
+
+};
+class BufferMapTestOp : public FilterOp {
+
+};
+
+#define CASE(p0, p1) \
+case p0: \
+test.op = make_unique<p1##TestOp>(test.ctx.get(), test.graphics.get(), test.outputWidth, test.outputHeight); \
+break
+
+int run(BufferTestMode mode) {
+    UnitTest test("buffer_" + toString(mode));
+    switch (mode) {
+        CASE(VERTEX, VertexBuffer);
+        //CASE(INDEX, IndexBuffer);
+        //CASE(UNIFORM, UniformBuffer);
+        //CASE(STORAGE, StorageBuffer);
+        //CASE(INSTANCING, Instancing);
+        //CASE(UPLOAD, BufferUpload);
+        //CASE(UPLOAD_SUBREGION, BufferUploadSubregion);
+        //CASE(DOWNLOAD_SUBREGION, BufferDownloadSubregion);
+        //CASE(MAP, BufferMap);
+    }
+    return test.run();
+}
+
+int main(int argc, char** argv) {
+	if (argc < 2) {
+        vector<BufferTestMode> testModes = {
+            VERTEX /*, INDEX, UNIFORM, STORAGE, INSTANCING,
+            UPLOAD, UPLOAD_SUBREGION, DOWNLOAD, DOWNLOAD_SUBREGION, MAP*/
+        };
+        int r = 0;
+        for (BufferTestMode m : testModes)
+            r |= run(m);
+        return r;
+	}
+    string bufferTestModeStr = argv[1];
+    return run(toBufferTestMode(bufferTestModeStr));
 	return 0;
 }
