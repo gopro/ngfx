320bc6ae2d68a1d3f22b86252fb2b371c81c43d3
Jeff Moguillansky <jmoguillansky@gopro.com>
2022-07-12 11:51:34 -0700

    test/texture: adding texture unit tests...

--- test/texture/Main.cpp
+++ test/texture/Main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
+#include "ngfx/drawOps/DrawTextureOp.h"
+#include "ngfx/graphics/TextureUtil.h"
+#include "test/common/UnitTest.h"
+using namespace ngfx;
+using namespace std;
 
-int main() {
-	return 0;
+class TextureTestOp : public FilterOp {
+public:
+    TextureTestOp(GraphicsContext* ctx, Graphics* graphics, int w, int h)
+        : FilterOp(ctx, graphics, w, h) {
+        SamplerDesc samplerDesc = {
+            FILTER_LINEAR, FILTER_LINEAR, FILTER_NEAREST,
+            CLAMP_TO_EDGE, CLAMP_TO_EDGE, CLAMP_TO_EDGE
+        };
+        texture.reset(TextureUtil::load(ctx, graphics, NGFX_TEST_DATA_DIR "/input.png", IMAGE_USAGE_SAMPLED_BIT, TEXTURE_TYPE_2D, false, 1, &samplerDesc));
+        drawTextureOp = make_unique<DrawTextureOp>(ctx, texture.get());
+    }
+    void draw(CommandBuffer* commandBuffer, Graphics* graphics) override {
+        drawTextureOp->draw(commandBuffer, graphics);
+    }
+    std::unique_ptr<DrawTextureOp> drawTextureOp;
+    std::unique_ptr<Texture> texture;
+};
+
+int run() {
+    UnitTest test("texture");
+    test.op = make_unique<TextureTestOp>(test.ctx.get(), test.graphics.get(),
+        1386, 1040);
+    return test.run();
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        return run();
+    }
 }
