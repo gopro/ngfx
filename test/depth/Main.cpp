4f5c497b174f21821feb064b79abfd9d053ac554
Jeff Moguillansky <jmoguillansky@gopro.com>
2022-08-02 16:12:28 -0700

    fixup! test/depth: add depth unit tests...

--- test/depth/Main.cpp
+++ test/depth/Main.cpp
@@ -3,7 +3,12 @@
 #include "ngfx/graphics/GraphicsCore.h"
 #include "ngfx/graphics/FilterOp.h"
 #include "ngfx/drawOps/DrawMeshOp.h"
+#include "ngfx/graphics/MeshUtil.h"
+#include "ngfx/graphics/Camera.h"
+#include "ngfx/graphics/Colors.h"
 #include "test/common/UnitTest.h"
+#include <glm/gtx/transform.hpp>
+#include <glm/gtx/component_wise.hpp>
 using namespace ngfx;
 using namespace std;
 
@@ -30,17 +35,45 @@ static PixelFormat toPixelFormat(const string& s) {
 class DepthModeTestOp : public FilterOp {
 public:
     DepthModeTestOp(GraphicsContext* ctx, Graphics* graphics, int w, int h, PixelFormat depthMode)
-        : FilterOp(ctx, graphics, w, h), depthMode(depthMode) {
+        : FilterOp(ctx, graphics, w, h, true), depthMode(depthMode) {
+        auto meshData = make_unique<MeshData>();
+        MeshUtil::importMesh(NGFX_DATA_DIR "/models/" "bunny.bin", *meshData);
+        drawMeshOp = make_unique<DrawMeshOp>(ctx, *meshData);
+        camera.reset(new Camera());
+        camera->zoom = -1.0f;
+        initModelMat(*meshData);
+        update();
+        ctx->clearColor = Color::Black;
+    }
+    void update() {
+        camera->update();
+        modelViewMat = camera->viewMat * modelMat;
+        modelViewInverseTransposeMat = transpose(inverse(modelViewMat));
+        float aspect = float(outputTexture->w) / outputTexture->h;
+        projMat = perspective(radians(60.0f), aspect, 0.1f, 100.0f);
+        modelViewProjMat = projMat * modelViewMat;
+        DrawMeshOp::LightData lightData;
+        drawMeshOp->update(modelViewMat, modelViewInverseTransposeMat, modelViewProjMat, lightData);
     }
     void draw(CommandBuffer* commandBuffer, Graphics* graphics) override {
         drawMeshOp->draw(commandBuffer, graphics);
     }
     std::unique_ptr<DrawMeshOp> drawMeshOp;
+    std::unique_ptr<Camera> camera;
     PixelFormat depthMode;
+    mat4 modelMat, modelViewMat, modelViewInverseTransposeMat, projMat, modelViewProjMat;
+private:
+    void initModelMat(MeshData& meshData) {
+        auto& b = meshData.bounds;
+        vec3 dim = b[1] - b[0];
+        vec3 center = 0.5f * (b[0] + b[1]);
+        float maxDim = compMax(dim);
+        modelMat = scale(vec3(1.0f / maxDim)) * translate(-center);
+    }
 };
 
 int run(PixelFormat depthFormat) {
-    UnitTest test("depth_" + toString(depthFormat));
+    UnitTest test("depth_" + toString(depthFormat), 256, 256, true);
     test.op = make_unique<DepthModeTestOp>(test.ctx.get(), test.graphics.get(),
         test.outputWidth, test.outputHeight, depthFormat);
     return test.run();
